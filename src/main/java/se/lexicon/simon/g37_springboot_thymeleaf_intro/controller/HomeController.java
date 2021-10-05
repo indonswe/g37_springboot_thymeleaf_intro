ebe04870cb0882aaebe8f2594b4e989712be60ee
Simon Elbrink <simon.elbrink@gmail.com>
2021-10-05 12:34:36 +0200

    Day two Part 1
    More links.
    Utext
    picture

--- src/main/java/se/lexicon/simon/g37_springboot_thymeleaf_intro/controller/HomeController.java
+++ src/main/java/se/lexicon/simon/g37_springboot_thymeleaf_intro/controller/HomeController.java
@@ -6,18 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     //http://localhost:8080/
     //http://localhost:8080/?message=Custom+Massage+From+Path
-    @GetMapping("/")
+    @GetMapping({"/", "/home"})
     public String home(
           @RequestParam(name = "message", defaultValue = "HELLO FROM THE INSIDE OF HOMECONTROLLER" )  String message,
             Model model){
 
         model.addAttribute("message",message);
+        model.addAttribute("hello", "hello this is another attribute");
+
 
         return "home";
     }
@@ -27,6 +30,11 @@ public class HomeController {
     @RequestMapping(value = "/secondpage", method = RequestMethod.GET)
     public String otherPage(){
         return "second";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
     }
 
 
