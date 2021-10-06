b5a5f91adac5f0c4a939776a7bc32800be5c6aef
Simon Elbrink <simon.elbrink@gmail.com>
2021-10-06 12:37:08 +0200

    Day 3 - Fragment + Create Form

--- src/main/java/se/lexicon/simon/g37_springboot_thymeleaf_intro/controller/ProductController.java
+++ src/main/java/se/lexicon/simon/g37_springboot_thymeleaf_intro/controller/ProductController.java
@@ -3,27 +3,27 @@ package se.lexicon.simon.g37_springboot_thymeleaf_intro.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import se.lexicon.simon.g37_springboot_thymeleaf_intro.model.Product;
 
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 
 @Controller
 public class ProductController {
 
-
-    private List<Product> products = Arrays.asList(
-
-            new Product("FirstOne", "Frideborg", BigDecimal.TEN),
-            new Product("Second", "Debbie", BigDecimal.valueOf(20))
+    private List<Product> products = new ArrayList<>(
+            Arrays.asList(
+                    new Product("FirstOne", "Frideborg", BigDecimal.TEN),
+                    new Product("Second", "Debbie", BigDecimal.valueOf(20))
+            )
     );
 
-    /*
+/*
     /products == findAll
     /products/add == create
     /products/{id} == findById
@@ -46,7 +46,6 @@ public class ProductController {
             return "redirect:/products";
         }
 
-
         return "product_view";
 
     }
@@ -54,11 +53,36 @@ public class ProductController {
     @GetMapping("/products")
     public String findAll(Model model){
 
+        //repo.findAll()
         model.addAttribute("products", products);
 
         return "products";
+    }
+
+    // 1. For the create view (the look)
+
+    @GetMapping("/products/add")
+    public String getCreateForm(Model model){
+
+        model.addAttribute("product", new Product());
+
+        return "product_create_form";
+    }
+
+
+    // 2. Processing the product to, actually save the data.
+    @PostMapping("/products/add/process")
+    public String processCreateForm(@ModelAttribute(name = "product") Product product){
+
+        product.setId(UUID.randomUUID().toString());
+        products.add(product);
+
+        return "redirect:/products/" + product.getId();
 
     }
+
+
+
 
 
 
